d787821ea69355daf376087b7f9edf20d4936b1e
Martina Brayley <67074230+mbrayley@users.noreply.github.com>
2024-04-29 15:06:40 +0100

    Updated code for generating DD

--- detection_mode_DD/DD.r
+++ detection_mode_DD/DD.r
@@ -1,10 +1,16 @@
+# ***************** Generating DD for detection mode dataset in RDS **************************
+
+# Martina Brayley
+# 29/04/2024
+
+# ______________________________________________________________________________________________________
 
 install.packages("pacman")
 install.packages("datadictionary")
-library(datadictionary)
+#library(datadictionary)
 
 library(pacman)
-library(magrittr)
+#library(magrittr)
 
 p_load(datadictionary,
        magrittr,
@@ -21,8 +27,8 @@ problems(detection_mode_df)
 
 str(detection_mode_df)
 
-factor_vars <- c("confirmed", "incident", "different_casum_row", "dmode_v2", 
-                 "cat_dmode_v2", "ancat_dmode_v2", "source_dm2",
+factor_vars <- c("different_casum_row", "dmode_v2", 
+                 "cat_dmode_v2", "ancat_dmode_v2", "SD_dg_first_screen", "source_dm2",
                  "dmode_v1", "cat_dmode_v1", "shim_dmode",
                  "gs_sd", "gs_sd_source", "reg_sd", "histo_sd") 
 
@@ -38,14 +44,15 @@ dmode_labels <- c(tcode = "participant identifier",
                   reginfo_clustrino = "unique row identifier",
                   report_groupdatesite = "unique row identifier",
                   report_cluster = "unique row identifier",
-                  confirmed = "confirmed cancer flag (source casummary)",
-                  incident = "incident cancer flag (source casummary)",
                   different_casum_row = "flag to indicate whether the row assigned dmode2 is different from the row assigned dmode1",
                   dmode_v2 = "fine categories of version 2 detection mode. Dmode_v2 is a combination of the different detection mode sources (screenign data, dmode v1, registry) and assigned by R algorithm (Martina)",
                   cat_dmode_v2 = "collapsed categories of dmode v2, including uncertain categories from dmode_v1",
                   ancat_dmode_v2 = "collapsed categories of dmode v2, excluding uncertain categories from dmode v1",
                   dmode_v2_eno = "screening episode number at which dmode v2 was derived",
+                  dens_screen_eno = "density related screening episode number at which dmode v2 was derived. For screen detected cases, this is the last attended routine negative screen before diagnosis",
                   dm2_screen_date_f = "date of screening episode at which dmode v2 was derived",
+                  dens_dm2_screen_date_f = "density related date of screening episode at which dmode v2 was derived. For screen detected cases, this is the last attended routine negative screen before diagnosis", 
+                  SD_dg_first_screen = "flag variable to indicate if screen detected case was diagnosed at the first screen", 
                   source_dm2 = "source od dmode v2",
                   diagdate_dm1_f = "diagnosis date associated with dmode v1",
                   dmode_v1 = "detection mode derived from screening data using more manual process (Louise)",
@@ -65,4 +72,4 @@ dmode_labels <- c(tcode = "participant identifier",
 
 # create DD 
 
-DD <- create_dictionary(df, var_labels = dmode_labels, file = "Q:/SHARED/USERS/MBrayley/Screening/detection_mode_DD/detection_mode_DD.xlsx")
+DD <- create_dictionary(df, var_labels = dmode_labels, file = "C:/Users/MBrayley/OneDrive - The Institute of Cancer Research/Work/GitHub/BC_detection_mode/detection_mode_DD.xlsx")
